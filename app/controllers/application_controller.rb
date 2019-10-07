6e1b2b4f2d482391ac749239319cbd688c737214
Jjingo Kisakye <sseguku@gmail.com>
2019-10-07 03:09:46 +0300

    removed security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::API
-    include ActionController::HttpAuthentication::Basic::ControllerMethods
-    include ActionController::HttpAuthentication::Token::ControllerMethods
+    # include ActionController::HttpAuthentication::Basic::ControllerMethods
+    # include ActionController::HttpAuthentication::Token::ControllerMethods
 
-    before_action :restrict_access
-    private
+    # before_action :restrict_access
+    # private
 
-    def restrict_access
-        authenticate_or_request_with_http_token do |token, options|
-            ApiKey.exists?(access_token: token)
-        end
-    end
+    # def restrict_access
+    #     authenticate_or_request_with_http_token do |token, options|
+    #         ApiKey.exists?(access_token: token)
+    #     end
+    # end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
     def index
-        render js: "VAZISTYLE API V1"
+        render plain: "VAZISTYLE API V1"
         #render template: 'api/v1/products'
     end
 end
